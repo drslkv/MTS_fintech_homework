368322436a85a9fde92f338fd095bcf3c4f47a2c
drslkv <92947583+drslkv@users.noreply.github.com>
2024-01-28 21:44:44 +0300

    Delete MTS_fintech_homework directory

--- MTS_fintech_homework/src/main/java/searchAnimal/SearchAnimalService.java
+++
@@ -1,9 +0,0 @@
-package searchAnimal;
-
-import descriptionAnimal.AbstractAnimal;
-
-public interface SearchAnimalService {
-    AbstractAnimal[] findLeapYearNames(AbstractAnimal[] animals);
-    AbstractAnimal[] findOlderAnimal(AbstractAnimal[] animals, int age);
-    void findDuplicate(AbstractAnimal[] animals);
-}
