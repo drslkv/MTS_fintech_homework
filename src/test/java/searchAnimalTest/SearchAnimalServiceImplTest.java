1b273784e1002b7a52fbcc5144d712977e9fd837
drslkv <92947583+drslkv@users.noreply.github.com>
2024-01-31 21:57:48 +0300

    дз-5 (правки)

--- src/test/java/searchAnimalTest/SearchAnimalServiceImplTest.java
+++ src/test/java/searchAnimalTest/SearchAnimalServiceImplTest.java
@@ -16,9 +16,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
